4988e99e382a6ab16a9269a1b707d2bbd9a9972f
Simon Moos <simon.moos95@gmail.com>
2020-07-27 17:53:38 +0200

    Make a custom final pass for the simple app

--- src/apps/SimpleApp.cpp
+++ src/apps/SimpleApp.cpp
@@ -1,6 +1,5 @@
 #include "SimpleApp.h"
 
-#include "rendering/nodes/FinalPostFxNode.h"
 #include "rendering/nodes/GBufferNode.h"
 #include "rendering/nodes/PickingNode.h"
 #include "rendering/nodes/SceneNode.h"
@@ -28,10 +27,42 @@ void SimpleApp::setup(RenderGraph& graph)
 
     graph.addNode<SceneNode>(scene());
     graph.addNode<PickingNode>(scene());
-    graph.addNode<GBufferNode>(scene());
+
     graph.addNode<ShadowMapNode>(scene());
+
+    graph.addNode<GBufferNode>(scene());
     graph.addNode<SlowForwardRenderNode>(scene());
-    graph.addNode<FinalPostFxNode>(scene());
+
+    graph.addNode("final", [](Registry& reg) {
+        std::vector<vec2> fullScreenTriangle { { -1, -3 }, { -1, 1 }, { 3, 1 } };
+        Buffer& vertexBuffer = reg.createBuffer(std::move(fullScreenTriangle), Buffer::Usage::Vertex, Buffer::MemoryHint::GpuOptimal);
+        VertexLayout vertexLayout = VertexLayout { sizeof(vec2), { { 0, VertexAttributeType::Float2, 0 } } };
+
+        BindingSet& sourceBindingSet = reg.createBindingSet({ { 0, ShaderStageFragment, reg.getTexture("forward", "color").value(), ShaderBindingType::TextureSampler } });
+        BindingSet& envBindingSet = reg.createBindingSet({ { 0, ShaderStageVertex, reg.getBuffer("scene", "camera") },
+                                                           { 1, ShaderStageFragment, reg.getTexture("scene", "environmentMap").value_or(&reg.createPixelTexture(vec4(1), true)), ShaderBindingType::TextureSampler },
+                                                           { 2, ShaderStageFragment, reg.getTexture("g-buffer", "depth").value(), ShaderBindingType::TextureSampler },
+                                                           { 3, ShaderStageFragment, reg.getBuffer("scene", "environmentData") } });
+
+        Shader shader = Shader::createBasicRasterize("final/simple.vert", "final/simple.frag");
+        RenderStateBuilder renderStateBuilder { reg.windowRenderTarget(), shader, vertexLayout };
+        renderStateBuilder.addBindingSet(sourceBindingSet).addBindingSet(envBindingSet);
+        renderStateBuilder.writeDepth = false;
+        renderStateBuilder.testDepth = false;
+
+        RenderState& renderState = reg.createRenderState(renderStateBuilder);
+
+        return [&](const AppState& appState, CommandList& cmdList) {
+            static float exposure = 0.45f;
+            ImGui::SliderFloat("Exposure", &exposure, 0.01f, 10.0f, "%.3f", 3.0f);
+
+            cmdList.beginRendering(renderState, ClearColor(0.5f, 0.1f, 0.5f), 1.0f);
+            cmdList.bindSet(sourceBindingSet, 0);
+            cmdList.bindSet(envBindingSet, 1);
+            cmdList.pushConstant(ShaderStageFragment, exposure);
+            cmdList.draw(vertexBuffer, 3);
+        };
+    });
 }
 
 void SimpleApp::update(float elapsedTime, float deltaTime)
