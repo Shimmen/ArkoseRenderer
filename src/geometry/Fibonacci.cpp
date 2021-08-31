609c99aa899e5f5b10f21f0d9aba54599128fff2
Simon Moos <simon.moos95@gmail.com>
2021-08-31 23:43:30 +0100

    Add Fibonacci-related maths utility functions

---
+++ src/geometry/Fibonacci.cpp
@@ -0,0 +1,39 @@
+#include "Fibonacci.h"
+
+namespace geometry {
+
+// Source: http://extremelearning.com.au/how-to-evenly-distribute-points-on-a-sphere-more-effectively-than-the-canonical-fibonacci-lattice/
+
+vec2 fibonacciLattice(uint32_t i, uint32_t n)
+{
+    ASSERT(i < n);
+
+    static const float goldenRatio = (1.0f + std::sqrt(5.0f)) / 2.0f;
+
+    float x = static_cast<float>(i) / goldenRatio;
+    x -= int(x);
+
+    float y = static_cast<float>(i) / static_cast<float>(n);
+
+    return vec2(x, y);
+}
+
+vec2 fibonacciSpiral(uint32_t i, uint32_t n)
+{
+    vec2 latticePoint = fibonacciLattice(i, n);
+    float angle = moos::TWO_PI * latticePoint.x;
+    float radius = sqrt(latticePoint.y);
+    return vec2(angle, radius);
+}
+
+vec3 sphericalFibonacci(uint32_t i, uint32_t n)
+{
+    vec2 latticePoint = fibonacciLattice(i, n);
+    float theta = moos::TWO_PI * latticePoint.x;
+    float phi = acos(2.0f * latticePoint.y - 1.0f);
+
+    float sinPhi = sin(phi);
+    return vec3(cos(theta) * sinPhi, sin(theta) * sinPhi, cos(phi));
+}
+
+}
---
+++ src/geometry/Fibonacci.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "utility/util.h"
+#include "utility/Profiling.h"
+#include <moos/core.h>
+#include <moos/vector.h>
+
+namespace geometry {
+
+    // Calculate the Fibonacci lattice point (index i out of n) in a unit square [0, 1)^2
+    vec2 fibonacciLattice(uint32_t i, uint32_t n);
+
+    // Calculate the Fibonacci spiral point (index i out of n) in a unit circle (r=1).
+    // Specified in polar coordinates (angle=[0, 2pi], radius=[0, 1].
+    vec2 fibonacciSpiral(uint32_t i, uint32_t n);
+
+    // Calculate the Fibonacci sphere / sphercial Fibonacci point (index i out of n) on the surface of a unit sphere (r=1)
+    vec3 sphericalFibonacci(uint32_t i, uint32_t n);
+
+}
