8d630a795d1dd2af7f20f0cfdd677cced871de8e
Simon Moos <simon.moos95@gmail.com>
2022-01-04 23:24:58 +0000

    Allow for more RenderTarget color targets

--- src/backend/base/RenderTarget.h
+++ src/backend/base/RenderTarget.h
@@ -23,6 +23,10 @@ public:
         Color1 = 1,
         Color2 = 2,
         Color3 = 3,
+        Color4 = 4,
+        Color5 = 5,
+        Color6 = 6,
+        Color7 = 7,
         Depth = UINT_MAX
     };
 
