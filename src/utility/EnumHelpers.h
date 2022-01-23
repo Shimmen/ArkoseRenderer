93cd25a63886468f9432c2ef998e1b7f6a41cd97
Simon Moos <simon.moos95@gmail.com>
2022-01-23 22:38:50 +0000

    Add ARKOSE_ENUM_CLASS_BIT_FLAGS macro

---
+++ src/utility/EnumHelpers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <type_traits>
+
+#define ARKOSE_ENUM_CLASS_BIT_FLAGS(T)                                                                                                                                               \
+    inline bool isSet(T a) { return static_cast<std::underlying_type_t<T>>(a) != 0; }                                                                                                \
+    inline T operator~(T a) { return static_cast<T>(~static_cast<std::underlying_type_t<T>>(a)); }                                                                                   \
+    inline T operator|(T a, T b) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(a) | static_cast<std::underlying_type_t<T>>(b)); }                                   \
+    inline T operator&(T a, T b) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(a) & static_cast<std::underlying_type_t<T>>(b)); }                                   \
+    inline T operator^(T a, T b) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(a) ^ static_cast<std::underlying_type_t<T>>(b)); }                                // \
+    //inline T& operator|=(T& a, T b) { return static_cast<T&>(static_cast<std::add_lvalue_reference_t<std::underlying_type_t<T>>>(a) |= static_cast<std::underlying_type_t<T>>(b)); } \
+    //inline T& operator&=(T& a, T b) { return static_cast<T&>(static_cast<std::add_lvalue_reference_t<std::underlying_type_t<T>>>(a) &= static_cast<std::underlying_type_t<T>>(b)); } \
+    //inline T& operator^=(T& a, T b) { return static_cast<T&>(static_cast<std::add_lvalue_reference_t<std::underlying_type_t<T>>>(a) ^= static_cast<std::underlying_type_t<T>>(b)); }
+
