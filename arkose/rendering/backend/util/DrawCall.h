21a30d392d67279728d2790e4722a4c6d83414a8
Simon Moos <simon.moos95@gmail.com>
2022-08-19 00:36:53 +0100

    Clean up DrawCallDescription type

--- arkose/rendering/backend/util/DrawCall.h
+++ arkose/rendering/backend/util/DrawCall.h
@@ -10,29 +10,41 @@ struct DrawCallDescription {
         NonIndexed,
     };
 
-    const Buffer* vertexBuffer;
-    const Buffer* indexBuffer;
-
     Type type;
-    uint32_t firstVertex { 0 };
-    uint32_t firstIndex { 0 };
 
+    const Buffer* vertexBuffer;
+
+    // for non-indexed draw calls
+    uint32_t firstVertex { 0 };
     uint32_t vertexCount { 0 };
+
+    // for indexed draw calls
+    uint32_t firstIndex { 0 };
+    uint32_t indexCount { 0 };
     int32_t vertexOffset { 0 };
 
-    IndexType indexType;
-    uint32_t indexCount { 0 };
+    const Buffer* indexBuffer { nullptr };
+    IndexType indexType { IndexType::UInt32 };
 
+    // for instancing
     uint32_t instanceCount { 1 };
     uint32_t firstInstance { 0 };
 
+    static DrawCallDescription makeSimple(Buffer& vertexBuffer, uint32_t vertexCount)
+    {
+        DrawCallDescription drawCall { .type = Type::NonIndexed,
+                                       .vertexBuffer = &vertexBuffer,
+                                       .vertexCount = vertexCount };
+        return drawCall;
+    }
+
     static DrawCallDescription makeSimpleIndexed(Buffer& vertexBuffer, Buffer& indexBuffer, uint32_t indexCount, IndexType indexType = IndexType::UInt32)
     {
-        DrawCallDescription drawCall { .vertexBuffer = &vertexBuffer,
+        DrawCallDescription drawCall { .type = Type::Indexed,
+                                       .vertexBuffer = &vertexBuffer,
+                                       .indexCount = indexCount,
                                        .indexBuffer = &indexBuffer,
-                                       .type = Type::Indexed,
-                                       .indexType = indexType,
-                                       .indexCount = indexCount };
+                                       .indexType = indexType };
         return drawCall;
     }
 };
