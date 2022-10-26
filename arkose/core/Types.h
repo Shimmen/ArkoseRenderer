6496bce0b38631a54d90865f06791a0d6f25a3de
Simon Moos <simon.moos95@gmail.com>
2022-10-26 23:04:29 +0100

    Expose short-form numbers types & add narrow_cast

--- arkose/core/Types.h
+++ arkose/core/Types.h
@@ -1,3 +1,27 @@
 #pragma once
 
-#include <cstdint> // for uint32_t and family
+#include "core/Assert.h"
+#include <ark/core.h>
+
+using i8 = ark::i8;
+using i16 = ark::i16;
+using i32 = ark::i32;
+using i64 = ark::i64;
+
+using u8 = ark::u8;
+using u16 = ark::u16;
+using u32 = ark::u32;
+using u64 = ark::u64;
+
+using f32 = float;
+using f64 = double;
+
+template<typename NarrowType, typename WideType>
+constexpr NarrowType narrow_cast(WideType);
+
+template<>
+constexpr u32 narrow_cast(u64 wideValue)
+{
+    ARKOSE_ASSERT(wideValue <= static_cast<u64>(UINT32_MAX));
+    return static_cast<u32>(wideValue);
+}
