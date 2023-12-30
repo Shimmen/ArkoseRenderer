578cae3dba486710ccf5f0c39994c6de5e773770
Simon Moos <simon.moos95@gmail.com>
2023-12-30 19:45:21 +0000

    Add ARKOSE_ASSERT implementation for which doesn't rely on stdlib assert()
    Only MSVC is properly implemented but it should work on all platforms.

--- arkcore/core/Assert.h
+++ arkcore/core/Assert.h
@@ -1,9 +1,48 @@
 #pragma once
 
-#include <cassert> // for assert
-#include <cstdlib> // for exit, for noreturn behaviour
+#include <ark/compiler.h> // for ARK_FORCE_INLINE
+#include <ark/debugger.h> // for ARK_DEBUG_BREAK()
+#include <cstdlib> // for exit()
+#include <fmt/format.h> // for fmt::format()
+#include <core/Logging.h> // for ARKOSE_LOG()
 
-#define ARKOSE_ASSERT(x) assert(x)
+#if defined(_MSC_VER)
+#include <Windows.h>
+#endif
+
+ARK_FORCE_INLINE void ArkoseAssertHandler(char const* assertion, char const* filename, int line)
+{
+    std::string assertionMessage = fmt::format("Assertion failed: '{}' ({} line {})", assertion, filename, line);
+    ARKOSE_LOG(Error, "{}", assertionMessage);
+
+#if defined(_MSC_VER)
+    if (IsDebuggerPresent()) {
+        std::string messageBoxMessage = fmt::format("Assertion failed: '{}'\nIn file {} on line {}\nDo you want to break?", assertion, filename, line);
+        switch (MessageBoxA(NULL, messageBoxMessage.c_str(), "Assertion failed!", MB_ABORTRETRYIGNORE)) {
+        case IDABORT:
+            exit(EXIT_FAILURE);
+            break;
+        case IDRETRY:
+            // TODO: Somehow issue the debug break inside the macro so that we don't include this function in the callstack
+            ARK_DEBUG_BREAK();
+            break;
+        case IDIGNORE:
+            // do nothing
+            break;
+        }
+    }
+#else
+    // TODO: Add better platform implementations!
+    ARK_DEBUG_BREAK();
+#endif
+}
+
+// NOTE: This triggers in both debug & release modes!
+#define ARKOSE_ASSERT(expression)                                 \
+    (void)(                                                       \
+        (!!(expression)) ||                                       \
+        (ArkoseAssertHandler(#expression, __FILE__, __LINE__), 0) \
+    )
 
 #define ASSERT_NOT_REACHED()  \
     do {                      \
