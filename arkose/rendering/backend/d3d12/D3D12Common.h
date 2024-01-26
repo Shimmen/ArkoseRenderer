307dae5cce0616b191f14459515dc7a409eac9f4
Simon Moos <simon.moos95@gmail.com>
2024-01-26 00:33:03 +0000

    Add various string<->wstring conversion functions

--- arkose/rendering/backend/d3d12/D3D12Common.h
+++ arkose/rendering/backend/d3d12/D3D12Common.h
@@ -9,3 +9,35 @@
 // For COM interfacing
 #include <wrl.h>
 using namespace Microsoft::WRL;
+
+// For various string conversions, as Windows likes utf16
+#include <locale>
+#include <codecvt>
+#include <string>
+
+inline std::wstring convertToWideString(char const* utf8string)
+{
+    static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    return converter.from_bytes(utf8string);
+}
+
+inline std::wstring convertToWideString(std::string_view utf8string)
+{
+    return convertToWideString(utf8string.data());
+}
+
+inline std::wstring convertToWideString(std::string const& utf8string)
+{
+    return convertToWideString(utf8string.data());
+}
+
+inline std::string convertFromWideString(wchar_t* utf16string)
+{
+    static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    return converter.to_bytes(utf16string);
+}
+
+inline std::string convertFromWideString(std::wstring utf16string)
+{
+    return convertFromWideString(utf16string.data());
+}
