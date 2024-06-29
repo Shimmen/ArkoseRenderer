f7e2ffa0eeafebb5a134f2cd2456897a09650729
Simon Moos <simon.moos95@gmail.com>
2024-06-29 17:04:54 +0100

    D3D12: Rewrite wide<->utf8 string conversions to use the Win32 API
    The C++ standard library functions I used previously are deprecated.

--- arkose/rendering/backend/d3d12/D3D12Common.h
+++ arkose/rendering/backend/d3d12/D3D12Common.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "core/Types.h"
+
 // Core D3D12 API
 #include <d3d12.h>
 
@@ -8,14 +10,21 @@
 using namespace Microsoft::WRL;
 
 // For various string conversions, as Windows likes utf16
-#include <locale>
-#include <codecvt>
-#include <string>
+#include <Windows.h>
 
 inline std::wstring convertToWideString(char const* utf8string)
 {
-    static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(utf8string);
+    size_t utf8Length = std::strlen(utf8string);
+    size_t requiredLength = ::MultiByteToWideChar(CP_UTF8, 0, utf8string, narrow_cast<int>(utf8Length), nullptr, 0);
+
+    if (requiredLength == 0) {
+        return std::wstring();
+    }
+
+    std::wstring wideString(requiredLength, L'\0');
+    ::MultiByteToWideChar(CP_UTF8, 0, utf8string, narrow_cast<int>(utf8Length), wideString.data(), narrow_cast<int>(wideString.length()));
+
+    return wideString;
 }
 
 inline std::wstring convertToWideString(std::string_view utf8string)
@@ -30,8 +39,17 @@ inline std::wstring convertToWideString(std::string const& utf8string)
 
 inline std::string convertFromWideString(wchar_t* utf16string)
 {
-    static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.to_bytes(utf16string);
+    size_t utf16Length = std::wcslen(utf16string);
+    size_t requiredLength = ::WideCharToMultiByte(CP_UTF8, 0, utf16string, narrow_cast<int>(utf16Length), nullptr, 0, nullptr, nullptr);
+
+    if (requiredLength == 0) {
+        return std::string();
+    }
+
+    std::string utf8string(requiredLength, '\0');
+    ::WideCharToMultiByte(CP_UTF8, 0, utf16string, narrow_cast<int>(utf16Length), utf8string.data(), narrow_cast<int>(utf8string.size()), nullptr, nullptr);
+
+    return utf8string;
 }
 
 inline std::string convertFromWideString(std::wstring utf16string)
