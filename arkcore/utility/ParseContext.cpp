06137b12b1164e61d4fe813ac66179101adce1c9
Simon Moos <simon.moos95@gmail.com>
2024-08-18 20:11:34 +0100

    Add support for more complex parsing in ParsingContext

--- arkcore/utility/ParseContext.cpp
+++ arkcore/utility/ParseContext.cpp
@@ -1,6 +1,7 @@
 #include "ParseContext.h"
 
 #include "core/Logging.h"
+#include <cctype>
 
 ParseContext::ParseContext(std::string const& fileType, std::string const& filePath)
     : m_fileType(fileType)
@@ -12,6 +13,114 @@ ParseContext::ParseContext(std::string const& fileType, std::string const& fileP
 bool ParseContext::isValid() const
 {
     return m_stream.good();
+}
+
+bool ParseContext::isEndOfFile() const
+{
+    int result = const_cast<ParseContext*>(this)->m_stream.peek();
+    return result == std::char_traits<char>::eof();
+}
+
+char ParseContext::peekNextCharacter() const
+{
+    int result = const_cast<ParseContext*>(this)->m_stream.peek();
+    if (result == std::char_traits<char>::eof()) {
+        return '\0';
+    }
+
+    return static_cast<char>(result);
+}
+
+char ParseContext::consumeCharacter()
+{
+    char tempBuffer[2];
+    m_stream.get(tempBuffer, sizeof(tempBuffer));
+    return tempBuffer[0];
+}
+
+void ParseContext::consumeWhitespace(int count)
+{
+    while (count != 0) {
+        if (std::isspace(peekNextCharacter())) {
+            m_stream.ignore();
+            count -= 1;
+        } else {
+            break;
+        }
+    }
+}
+
+void ParseContext::consumeNewline(int count, char newlineChar)
+{
+    while (count != 0) {
+        if (peekNextCharacter() == newlineChar) {
+            m_stream.ignore();
+            count -= 1;
+        } else {
+            break;
+        }
+    }
+}
+
+std::optional<std::string> ParseContext::consumeStandardSymbol()
+{
+    std::string symbol = "";
+
+    auto isAlphaOrUnderscore = [](char c) {
+        return (c >= 'a' && c <= 'z')
+            || (c >= 'A' && c <= 'Z')
+            || (c == '_');
+    };
+
+    auto isAlphanumericOrUnderscore = [&](char c) {
+        return isAlphaOrUnderscore(c)
+            || (c >= '0' && c <= '9');
+    };
+
+    char c = peekNextCharacter();
+    if (isAlphaOrUnderscore(c)) {
+        symbol.push_back(consumeCharacter());
+    } else {
+        return std::nullopt;
+    }
+
+    while (true) {
+        c = peekNextCharacter();
+        if (isAlphanumericOrUnderscore(c)) {
+            symbol.push_back(consumeCharacter());
+        } else {
+            break;
+        }
+    }
+
+    return symbol;
+}
+
+std::optional<std::string> ParseContext::consumeString(char stringDelimiter)
+{
+    if (peekNextCharacter() == stringDelimiter) {
+        consumeCharacter();
+    } else {
+        return std::nullopt;
+    }
+
+    std::string stringValue = "";
+    while (true) {
+        char c = peekNextCharacter();
+        if (c == '\n') {
+            // TODO: Handle newline during string parsing as an error
+            // (but, for now, let's just end the string here on newline)
+            break;
+        } else if (c == stringDelimiter) {
+            consumeCharacter();
+            break;
+        } else {
+            stringValue.push_back(c);
+            consumeCharacter();
+        }
+    }
+
+    return stringValue;
 }
 
 std::string ParseContext::nextLine()
--- arkcore/utility/ParseContext.h
+++ arkcore/utility/ParseContext.h
@@ -10,6 +10,20 @@ public:
 
     bool isValid() const;
 
+    bool isEndOfFile() const;
+
+    char peekNextCharacter() const;
+    char consumeCharacter();
+
+    void consumeWhitespace(int count = -1);
+    void consumeNewline(int count, char newlineChar = '\n');
+
+    // a "standard" symbol in the regex format /[_a-zA-Z][_a-zA-Z0-9]*/
+    std::optional<std::string> consumeStandardSymbol();
+
+    // a string begining and ending with the string delimiter and containing any character except the delimiter or newline
+    std::optional<std::string> consumeString(char stringDelimiter);
+
     std::string nextLine();
 
     std::optional<int> nextAsInt();
