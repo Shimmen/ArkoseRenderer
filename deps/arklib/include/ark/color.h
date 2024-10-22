468aeb75306f8b4ccb3381f73a1f22a43c606134
Simon Moos <simon.moos95@gmail.com>
2024-10-22 19:46:52 +0100

    arklib: convert color.h to always & explicitly use f32
    The `Float` type (note large F) can be of any bit depth, but in for color operations anything
    beyond 32-bit floats is massively overkill.
    
    This change also fixes hundreds of warnings related to implicit conversions from
    double to float.

--- deps/arklib/include/ark/color.h
+++ deps/arklib/include/ark/color.h
@@ -29,37 +29,37 @@
 namespace ark {
 
 // NOTE: There is no one standard for this, but I will use this range
-constexpr Float visibleLightMinWavelength = 380.0;
-constexpr Float visibleLightMaxWavelength = 780.0;
-constexpr Float visibleLightWavelengthRangeLength = visibleLightMaxWavelength - visibleLightMinWavelength;
+constexpr f32 visibleLightMinWavelength = 380.0f;
+constexpr f32 visibleLightMaxWavelength = 780.0f;
+constexpr f32 visibleLightWavelengthRangeLength = visibleLightMaxWavelength - visibleLightMinWavelength;
 constexpr size_t visibleLightWavelengthRangeSteps = static_cast<size_t>(visibleLightWavelengthRangeLength) + 1;
 
 namespace standardIlluminant {
-    constexpr Float D65 = 6504.0;
+    constexpr f32 D65 = 6504.0f;
 }
 
 namespace colorTemperature {
     // From or inspired by https://en.wikipedia.org/wiki/Color_temperature#Categorizing_different_lighting
-    constexpr Float candle = 1850.0;
-    constexpr Float incandescentBulb = 2400.0;
-    constexpr Float studioLight = 3200.0;
-    constexpr Float fluorescentBulb = 5000.0;
+    constexpr f32 candle = 1850.0f;
+    constexpr f32 incandescentBulb = 2400.0f;
+    constexpr f32 studioLight = 3200.0f;
+    constexpr f32 fluorescentBulb = 5000.0f;
 }
 
-inline Float blackBodyRadiation(Float temperature, Float wavelengthInNanometer)
+inline f32 blackBodyRadiation(f32 temperature, f32 wavelengthInNanometer)
 {
     // From https://www.shadertoy.com/view/MstcD7 but it's also trivial to
     // reconstruct from Planck's Law: https://en.wikipedia.org/wiki/Planck%27s_law
 
-    Float h = 6.6e-34; // Planck constant
-    Float kb = 1.4e-23; // Boltzmann constant
-    Float c = 3e8; // Speed of light
+    f32 h = 6.6e-34f; // Planck constant
+    f32 kb = 1.4e-23f; // Boltzmann constant
+    f32 c = 3e8f; // Speed of light
 
-    Float w = wavelengthInNanometer / 1e9;
-    const Float& t = temperature;
+    f32 w = wavelengthInNanometer / 1e9f;
+    const f32& t = temperature;
 
-    Float w5 = w * w * w * w * w;
-    Float o = 2.0 * h * (c * c) / (w5 * (std::exp((h * c) / (w * kb * t)) - 1.0));
+    f32 w5 = w * w * w * w * w;
+    f32 o = 2.0f * h * (c * c) / (w5 * (std::exp((h * c) / (w * kb * t)) - 1.0f));
 
     return o;
 }
@@ -72,124 +72,124 @@ namespace colorspace {
 
         // xyz (bar) fits from Listing 1 of https://research.nvidia.com/publication/simple-analytic-approximations-cie-xyz-color-matching-functions
 
-        inline Float xBarFit(Float wave)
+        inline f32 xBarFit(f32 wave)
         {
-            Float t1 = (wave - 442.0) * ((wave < 442.0) ? 0.0624 : 0.0374);
-            Float t2 = (wave - 599.8) * ((wave < 599.8) ? 0.0264 : 0.0323);
-            Float t3 = (wave - 501.1) * ((wave < 501.1) ? 0.0490 : 0.0382);
-            return 0.362 * std::exp(-0.5 * t1 * t1) + 1.056 * std::exp(-0.5 * t2 * t2) - 0.065 * std::exp(-0.5 * t3 * t3);
+            f32 t1 = (wave - 442.0f) * ((wave < 442.0f) ? 0.0624f : 0.0374f);
+            f32 t2 = (wave - 599.8f) * ((wave < 599.8f) ? 0.0264f : 0.0323f);
+            f32 t3 = (wave - 501.1f) * ((wave < 501.1f) ? 0.0490f : 0.0382f);
+            return 0.362f * std::exp(-0.5f * t1 * t1) + 1.056f * std::exp(-0.5f * t2 * t2) - 0.065f * std::exp(-0.5f * t3 * t3);
         }
 
-        inline Float yBarFit(Float wave)
+        inline f32 yBarFit(f32 wave)
         {
-            Float t1 = (wave - 568.8) * ((wave < 568.8) ? 0.0213 : 0.0247);
-            Float t2 = (wave - 530.9) * ((wave < 530.9) ? 0.0613 : 0.0322);
-            return 0.821 * std::exp(-0.5 * t1 * t1) + 0.286 * std::exp(-0.5 * t2 * t2);
+            f32 t1 = (wave - 568.8f) * ((wave < 568.8f) ? 0.0213f : 0.0247f);
+            f32 t2 = (wave - 530.9f) * ((wave < 530.9f) ? 0.0613f : 0.0322f);
+            return 0.821f * std::exp(-0.5f * t1 * t1) + 0.286f * std::exp(-0.5f * t2 * t2);
         }
 
-        inline Float zBarFit(Float wave)
+        inline f32 zBarFit(f32 wave)
         {
-            Float t1 = (wave - 437.0) * ((wave < 437.0) ? 0.0845 : 0.0278);
-            Float t2 = (wave - 459.0) * ((wave < 459.0) ? 0.0385 : 0.0725);
-            return 1.217 * std::exp(-0.5 * t1 * t1) + 0.681 * std::exp(-0.5 * t2 * t2);
+            f32 t1 = (wave - 437.0f) * ((wave < 437.0f) ? 0.0845f : 0.0278f);
+            f32 t2 = (wave - 459.0f) * ((wave < 459.0f) ? 0.0385f : 0.0725f);
+            return 1.217f * std::exp(-0.5f * t1 * t1) + 0.681f * std::exp(-0.5f * t2 * t2);
         }
 
-        inline Float photometricCurveFit(Float wave)
+        inline f32 photometricCurveFit(f32 wave)
         {
             return yBarFit(wave);
         }
 
-        inline vec3 fromSingleWavelength(Float power, Float wavelength)
+        inline vec3 fromSingleWavelength(f32 power, f32 wavelength)
         {
-            Float X = xBarFit(wavelength);
-            Float Y = yBarFit(wavelength);
-            Float Z = zBarFit(wavelength);
+            f32 X = xBarFit(wavelength);
+            f32 Y = yBarFit(wavelength);
+            f32 Z = zBarFit(wavelength);
             return power * vec3(X, Y, Z);
         }
 
-        inline vec3 fromBlackBodyTemperature(Float temperature, int numSteps = 100)
+        inline vec3 fromBlackBodyTemperature(f32 temperature, int numSteps = 100)
         {
-            Float stepWidth = visibleLightWavelengthRangeLength / static_cast<Float>(numSteps);
+            f32 stepWidth = visibleLightWavelengthRangeLength / static_cast<f32>(numSteps);
 
             vec3 XYZ = { 0, 0, 0 };
             for (int i = 0; i < numSteps; i++) {
-                Float mix = static_cast<Float>(i) / static_cast<Float>(numSteps - 1);
-                Float wavelength = lerp(visibleLightMinWavelength, visibleLightMaxWavelength, mix);
-                Float power = blackBodyRadiation(temperature, wavelength);
+                f32 mix = static_cast<f32>(i) / static_cast<f32>(numSteps - 1);
+                f32 wavelength = lerp(visibleLightMinWavelength, visibleLightMaxWavelength, mix);
+                f32 power = blackBodyRadiation(temperature, wavelength);
                 XYZ += fromSingleWavelength(power, wavelength) * stepWidth;
             }
 
             return XYZ;
         }
 
-        constexpr vec3 from_xyY(vec2 xy, Float Y)
+        constexpr vec3 from_xyY(vec2 xy, f32 Y)
         {
-            Float scale = Y / xy.y;
-            Float X = scale * xy.x;
-            Float Z = scale * (1.0 - xy.x - xy.y);
+            f32 scale = Y / xy.y;
+            f32 X = scale * xy.x;
+            f32 Z = scale * (1.0f - xy.x - xy.y);
             return { X, Y, Z };
         }
 
         constexpr vec2 to_xy(vec3 XYZ)
         {
-            Float sum = XYZ.x + XYZ.y + XYZ.z;
-            Float x = XYZ.x / sum;
-            Float y = XYZ.y / sum;
+            f32 sum = XYZ.x + XYZ.y + XYZ.z;
+            f32 x = XYZ.x / sum;
+            f32 y = XYZ.y / sum;
             return { x, y };
         }
 
         constexpr vec3 to_xyz(vec3 XYZ)
         {
             vec2 xy = to_xy(XYZ);
-            Float z = 1.0 - xy.x - xy.y;
+            f32 z = 1.0f - xy.x - xy.y;
             return { xy.x, xy.y, z };
         }
 
     } // namespace XYZ
 
     const mat3 XYZ_from_sRGB = mat3(
-        { 0.4123865632529917, 0.21263682167732384, 0.019330620152483987 },
-        { 0.35759149092062537, 0.7151829818412507, 0.11919716364020845 },
-        { 0.18045049120356368, 0.07218019648142547, 0.9503725870054354 });
+        { 0.4123865632529917f, 0.21263682167732384f, 0.019330620152483987f },
+        { 0.35759149092062537f, 0.7151829818412507f, 0.11919716364020845f },
+        { 0.18045049120356368f, 0.07218019648142547f, 0.9503725870054354f });
 
     const mat3 sRGB_from_XYZ = mat3(
-        { 3.2410032329763587, -0.9692242522025166, 0.055639419851975444 },
-        { -1.5373989694887855, 1.875929983695176, -0.20401120612390997 },
-        { -0.4986158819963629, 0.041554226340084724, 1.0571489771875335 });
+        { 3.2410032329763587f, -0.9692242522025166f, 0.055639419851975444f },
+        { -1.5373989694887855f, 1.875929983695176f, -0.20401120612390997f },
+        { -0.4986158819963629f, 0.041554226340084724f, 1.0571489771875335f });
 
     namespace sRGB {
 
         // Primaries & white point from https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/VkColorSpaceKHR.html
 
         const vec2 primaries[3] = {
-            vec2(0.64, 0.33),
-            vec2(0.30, 0.60),
-            vec2(0.15, 0.06)
+            vec2(0.64f, 0.33f),
+            vec2(0.30f, 0.60f),
+            vec2(0.15f, 0.06f)
         };
 
-        const vec2 whitePoint = vec2(0.3127, 0.3290);
-        const Float whitePointIlluminant = standardIlluminant::D65;
+        const vec2 whitePoint = vec2(0.3127f, 0.3290f);
+        const f32 whitePointIlluminant = standardIlluminant::D65;
 
-        constexpr Float luminance(const vec3& color)
+        constexpr f32 luminance(const vec3& color)
         {
-            constexpr vec3 Y = vec3(0.2126, 0.7152, 0.0722);
+            constexpr vec3 Y = vec3(0.2126f, 0.7152f, 0.0722f);
             return dot(color, Y);
         }
 
-        constexpr Float gammaEncode(Float linear)
+        constexpr f32 gammaEncode(f32 linear)
         {
             // (i.e. convert from linear sRGB to gamma-encoded sRGB)
-            return (linear < 0.0031308)
-                ? 12.92 * linear
-                : 1.055 * std::pow(linear, 1.0 / 2.4) - 0.055;
+            return (linear < 0.0031308f)
+                ? 12.92f * linear
+                : 1.055f * std::pow(linear, 1.0f / 2.4f) - 0.055f;
         }
 
-        constexpr Float gammaDecode(Float encoded)
+        constexpr f32 gammaDecode(f32 encoded)
         {
             // (i.e. convert from gamma-encoded sRGB to linear sRGB)
-            return (encoded < 0.04045)
-                ? encoded / 12.92
-                : std::pow((encoded + 0.055) / 1.055, 2.4);
+            return (encoded < 0.04045f)
+                ? encoded / 12.92f
+                : std::pow((encoded + 0.055f) / 1.055f, 2.4f);
         }
 
         constexpr vec3 gammaEncode(const vec3& linear)
@@ -202,7 +202,7 @@ namespace colorspace {
             return { gammaDecode(encoded.x), gammaDecode(encoded.y), gammaDecode(encoded.z) };
         }
 
-        inline vec3 fromBlackBodyTemperature(Float temperature, int numSteps = 100)
+        inline vec3 fromBlackBodyTemperature(f32 temperature, int numSteps = 100)
         {
             vec3 XYZ = XYZ::fromBlackBodyTemperature(temperature, numSteps);
             vec3 sRGB = sRGB_from_XYZ * XYZ;
@@ -212,56 +212,56 @@ namespace colorspace {
     } // namespace sRGB
 
     const mat3 XYZ_from_Rec2020 = mat3(
-        { 0.636953507, 0.262698339, 0.0280731358 },
-        { 0.144619185, 0.678008766, 0.0280731358 },
-        { 0.168855854, 0.0592928953, 1.06082723 });
+        { 0.636953507f, 0.262698339f, 0.0280731358f },
+        { 0.144619185f, 0.678008766f, 0.0280731358f },
+        { 0.168855854f, 0.0592928953f, 1.06082723f });
 
     const mat3 Rec2020_from_XYZ = mat3(
-        { 1.71666343, -0.66667384, 0.01764248 },
-        { -0.35567332, 1.61645574, -0.04277698 },
-        { -0.25336809, 0.0157683, 0.94224328 });
+        { 1.71666343f, -0.66667384f, 0.01764248f },
+        { -0.35567332f, 1.61645574f, -0.04277698f },
+        { -0.25336809f, 0.0157683f, 0.94224328f });
 
     namespace Rec2020 {
 
         // Primaries & white point from https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/VkColorSpaceKHR.html
 
         const vec2 primaries[3] = {
-            vec2(0.708, 0.292),
-            vec2(0.17, 0.797),
-            vec2(0.131, 0.046)
+            vec2(0.708f, 0.292f),
+            vec2(0.17f, 0.797f),
+            vec2(0.131f, 0.046f)
         };
 
-        const vec2 whitePoint = vec2(0.3127, 0.3290);
-        const Float whitePointIlluminant = standardIlluminant::D65;
+        const vec2 whitePoint = vec2(0.3127f, 0.3290f);
+        const f32 whitePointIlluminant = standardIlluminant::D65;
 
-        inline Float encodePQfromLinear(Float x, Float maxNits)
+        inline f32 encodePQfromLinear(f32 x, f32 maxNits)
         {
             // From https://www.khronos.org/registry/DataFormat/specs/1.3/dataformat.1.3.html#TRANSFER_PQ_IEOTF
 
-            ARK_ASSERT(x >= 0.0 && x <= 1.0);
-            ARK_ASSERT(maxNits > 0.0 && maxNits <= 10'000.0);
+            ARK_ASSERT(x >= 0.0f && x <= 1.0f);
+            ARK_ASSERT(maxNits > 0.0f && maxNits <= 10'000.0f);
 
             // Scale it so that x=1 is equivalent to maxNits on a calibrated display. This is done
             // since 10'000 is not actually possible to achieve in practice on any modern displays.
-            Float L0 = x * maxNits / 10'000.0;
+            f32 L0 = x * maxNits / 10'000.0f;
 
-            constexpr Float c1 = 107.0 / 128.0;
-            constexpr Float c2 = 2413.0 / 128.0;
-            constexpr Float c3 = 2392.0 / 128.0;
-            constexpr Float m1 = 1305.0 / 8192.0;
-            constexpr Float m2 = 2523.0 / 32.0;
+            constexpr f32 c1 = 107.0f / 128.0f;
+            constexpr f32 c2 = 2413.0f / 128.0f;
+            constexpr f32 c3 = 2392.0f / 128.0f;
+            constexpr f32 m1 = 1305.0f / 8192.0f;
+            constexpr f32 m2 = 2523.0f / 32.0f;
 
-            Float L = std::pow(L0, m1);
-            Float V = std::pow((c1 + c2 * L) / (1.0 + c3 * L), m2);
+            f32 L = std::pow(L0, m1);
+            f32 V = std::pow((c1 + c2 * L) / (1.0f + c3 * L), m2);
 
             return V;
         }
 
-        inline vec3 encodePQfromLinear(vec3 rgb, Float maxNits = 1500.0)
+        inline vec3 encodePQfromLinear(vec3 rgb, f32 maxNits = 1500.0f)
         {
-            Float r = encodePQfromLinear(rgb.x, maxNits);
-            Float g = encodePQfromLinear(rgb.y, maxNits);
-            Float b = encodePQfromLinear(rgb.z, maxNits);
+            f32 r = encodePQfromLinear(rgb.x, maxNits);
+            f32 g = encodePQfromLinear(rgb.y, maxNits);
+            f32 b = encodePQfromLinear(rgb.z, maxNits);
             return { r, g, b };
         }
 
@@ -276,20 +276,20 @@ namespace colorspace {
 
         // sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
         const mat3 inputMatrix = mat3(
-            { 0.59719, 0.07600, 0.02840 },
-            { 0.35458, 0.90834, 0.13383 },
-            { 0.04823, 0.01566, 0.83777 });
+            { 0.59719f, 0.07600f, 0.02840f },
+            { 0.35458f, 0.90834f, 0.13383f },
+            { 0.04823f, 0.01566f, 0.83777f });
 
         // ODT_SAT => XYZ => D60_2_D65 => sRGB
         const mat3 outputMatrix = mat3(
-            { 1.60475, -0.10208, -0.00327 },
-            { -0.53108, 1.10813, -0.07276 },
-            { -0.07367, -0.00605, 1.07602 });
+            { 1.60475f, -0.10208f, -0.00327f },
+            { -0.53108f, 1.10813f, -0.07276f },
+            { -0.07367f, -0.00605f, 1.07602f });
 
         constexpr vec3 RRTAndODTFit(vec3 v)
         {
-            vec3 a = v * (v + 0.0245786) - 0.000090537;
-            vec3 b = v * (v * 0.983729 + 0.4329510) + 0.238081;
+            vec3 a = v * (v + 0.0245786f) - 0.000090537f;
+            vec3 b = v * (v * 0.983729f + 0.4329510f) + 0.238081f;
             return a / b;
         }
 
@@ -298,7 +298,7 @@ namespace colorspace {
             color = inputMatrix * color;
             color = RRTAndODTFit(color);
             color = outputMatrix * color;
-            color = clamp(color, vec3(0.0), vec3(1.0));
+            color = clamp(color, vec3(0.0f), vec3(1.0f));
             return color;
         }
 
@@ -311,63 +311,63 @@ namespace colorspace {
             // From https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
 
             // make sure H is in range [0, 360] degrees
-            Float h = HSV.x;
-            h = std::fmod(h, 360.0);
+            f32 h = HSV.x;
+            h = std::fmod(h, 360.0f);
             h += 360.0;
-            h = std::fmod(h, 360.0);
+            h = std::fmod(h, 360.0f);
 
-            const Float& s = HSV.y;
-            const Float& v = HSV.z;
+            const f32& s = HSV.y;
+            const f32& v = HSV.z;
 
-            Float c = v * s;
-            Float hPrim = h / 60.0;
-            Float x = c * (1.0 - std::abs(std::fmod(hPrim, 2.0) - 1.0));
-            Float m = v - c;
+            f32 c = v * s;
+            f32 hPrim = h / 60.0f;
+            f32 x = c * (1.0f - std::abs(std::fmod(hPrim, 2.0f) - 1.0f));
+            f32 m = v - c;
 
-            if (hPrim <= 1.0)
+            if (hPrim <= 1.0f)
                 return { c + m, x + m, m };
-            if (hPrim <= 2.0)
+            if (hPrim <= 2.0f)
                 return { x + m, c + m, m };
-            if (hPrim <= 3.0)
+            if (hPrim <= 3.0f)
                 return { m, c + m, x + m };
-            if (hPrim <= 4.0)
+            if (hPrim <= 4.0f)
                 return { m, x + m, c + m };
-            if (hPrim <= 5.0)
+            if (hPrim <= 5.0f)
                 return { x + m, m, c + m };
-            if (hPrim <= 6.0)
+            if (hPrim <= 6.0f)
                 return { c + m, m, x + m };
 
             ARK_ASSERT(false);
-            return { 0.0, 0.0, 0.0 };
+            return { 0.0f, 0.0f, 0.0f };
         }
 
         constexpr vec3 fromRGB(vec3 RGB)
         {
             // From https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
 
-            RGB = clamp(RGB, vec3(0.0), vec3(1.0));
-            const Float& r = RGB.x;
-            const Float& g = RGB.y;
-            const Float& b = RGB.z;
+            RGB = clamp(RGB, vec3(0.0f), vec3(1.0f));
+            const f32& r = RGB.x;
+            const f32& g = RGB.y;
+            const f32& b = RGB.z;
 
-            Float xMax = std::max(r, std::max(g, b));
-            Float xMin = std::min(r, std::min(g, b));
-            Float c = xMax - xMin;
+            f32 xMax = std::max(r, std::max(g, b));
+            f32 xMin = std::min(r, std::min(g, b));
+            f32 c = xMax - xMin;
 
-            const Float& v = xMax;
-            Float s = (v == 0.0)
-                ? 0.0
+            const f32& v = xMax;
+            f32 s = (v == 0.0f)
+                ? 0.0f
                 : c / v;
 
-            Float h = 0.0;
-            if (c == 0.0)
-                h = 0.0;
+            f32 h = 0.0f;
+            if (c == 0.0f)
+                h = 0.0f;
             else if (v == r)
-                h = 60.0 * (g - b) / c;
+                h = 60.0f * (g - b) / c;
             else if (v == g)
-                h = 60.0 * (2.0 + (b - r) / c);
+                h = 60.0f * (2.0f + (b - r) / c);
             else if (v == b)
-                h = 60.0 * (4.0 + (r - g) / c);
+                h = 60.0f * (4.0f + (r - g) / c);
             else
                 ARK_ASSERT(false);
 
