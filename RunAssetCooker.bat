73cfa10b0f77da6f6a2b262e3decffe44c888ec3
Simon Moos <simon.moos95@gmail.com>
2025-03-19 22:31:31 +0000

    Tools: add script for running asset cooker & prepare its dependencies

---
+++ RunAssetCooker.bat
@@ -0,0 +1,35 @@
+@echo off
+
+echo ====================================
+echo == Checking for AssetCooker exec. ==
+echo ====================================
+echo(
+
+IF NOT EXIST "tools/bin/AssetCooker.exe" (
+    echo ERROR: AssetCooker executable not found!
+    echo(
+    echo Please download, build, and copy the executable into this directory! This is temporary..
+    echo You can find the code at https://github.com/jlaumon/AssetCooker
+    echo(
+    pause
+    exit /b 1
+) ELSE (
+    echo AssetCooker executable found!
+)
+
+echo(
+echo ====================================
+echo == Ensure GltfImportTool is built ==
+echo ====================================
+echo(
+
+cmake --build build --target GltfImportTool --config Release
+
+echo(
+echo ====================================
+echo ====== Launching AssetCooker! ======
+echo ====================================
+
+pushd "tools/bin"
+start AssetCooker.exe
+popd
