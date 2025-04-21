a86ddbff6744c316f80df32561b1a1497c610c39
Simon Moos <simon.moos95@gmail.com>
2025-04-21 22:28:57 +0100

    Make DirectionalLightShadowNode use the depth-only meshlet drawing code

--- arkose/rendering/nodes/DirectionalLightShadowNode.cpp
+++ arkose/rendering/nodes/DirectionalLightShadowNode.cpp
@@ -1,12 +1,7 @@
 #include "DirectionalLightShadowNode.h"
 
-#include "core/math/Frustum.h"
-#include "core/parallel/ParallelFor.h"
 #include "rendering/GpuScene.h"
-#include "rendering/RenderPipeline.h"
-#include "scene/lights/Light.h"
-#include "rendering/util/ScopedDebugZone.h"
-#include "utility/Profiling.h"
+#include "scene/lights/DirectionalLight.h"
 #include <imgui.h>
 
 void DirectionalLightShadowNode::drawGui()
@@ -16,16 +11,6 @@ void DirectionalLightShadowNode::drawGui()
 
 RenderPipelineNode::ExecuteCallback DirectionalLightShadowNode::construct(GpuScene& scene, Registry& reg)
 {
-    //
-    // NOTE: We shouldn't rely on TAA to clean up the noise produced by this as the noise messes with history samples.
-    // We should ensure we denoise it before we pass it on, and let TAA just smooth out the last little bit.
-    //
-
-    // TODO: Figure out a more robust way of figuring out if we have written depth as required
-    //if (!(reg.hasPreviousNode("Prepass") || reg.hasPreviousNode("Forward"))) {
-    //    ARKOSE_LOG(Fatal, "Directional light shadow needs scene depth information, can't progress");
-    //}
-
     m_shadowMap = &reg.createTexture2D({ 8192, 8192 },
                                        Texture::Format::Depth32F,
                                        Texture::Filters::linear(),
@@ -33,76 +18,27 @@ RenderPipelineNode::ExecuteCallback DirectionalLightShadowNode::construct(GpuSce
                                        ImageWrapModes::clampAllToEdge());
     reg.publish("DirectionalLightShadowMap", *m_shadowMap);
 
-    RenderTarget& shadowMapRenderTarget = reg.createRenderTarget({ { RenderTarget::AttachmentType::Depth, m_shadowMap } });
+    return MeshletDepthOnlyRenderNode::construct(scene, reg);
+}
 
-    Shader shadowMapShader = Shader::createVertexOnly("shadow/shadowMap.vert");
+vec2 DirectionalLightShadowNode::depthBiasParameters(GpuScene& scene) const
+{
+    DirectionalLight* light = scene.scene().firstDirectionalLight();
+    return { light->constantBias(), light->slopeBias() };
+}
 
-    RenderStateBuilder renderStateBuilder { shadowMapRenderTarget, shadowMapShader, { scene.vertexManager().positionVertexLayout() } };
-    renderStateBuilder.enableDepthBias = true;
-    renderStateBuilder.stateBindings().at(0, *reg.getBindingSet("SceneObjectSet"));
-    RenderState& renderState = reg.createRenderState(renderStateBuilder);
+mat4 DirectionalLightShadowNode::calculateViewProjectionMatrix(GpuScene& scene) const
+{
+    DirectionalLight* light = scene.scene().firstDirectionalLight();
+    return light->viewProjection();
+}
 
-    return [&](const AppState& appState, CommandList& cmdList, UploadBuffer& uploadBuffer) {
+geometry::Frustum DirectionalLightShadowNode::calculateCullingFrustum(GpuScene& scene) const
+{
+    return geometry::Frustum::createFromProjectionMatrix(calculateViewProjectionMatrix(scene));
+}
 
-        DirectionalLight* light = scene.scene().firstDirectionalLight();
-        if (light == nullptr || !light->castsShadows()) {
-            return;
-        }
-
-        mat4 lightProjectionFromWorld = light->viewProjection();
-        auto lightFrustum = geometry::Frustum::createFromProjectionMatrix(lightProjectionFromWorld);
-        mat4 lightProjectionFromView = lightProjectionFromWorld * inverse(scene.camera().viewMatrix());
-
-        cmdList.beginRendering(renderState, ClearValue::blackAtMaxDepth());
-
-        cmdList.setNamedUniform<mat4>("lightProjectionFromWorld", lightProjectionFromWorld);
-
-        cmdList.bindVertexBuffer(scene.vertexManager().positionVertexBuffer(), scene.vertexManager().positionVertexLayout().packedVertexSize(), 0);
-        cmdList.bindIndexBuffer(scene.vertexManager().indexBuffer(), scene.vertexManager().indexType());
-
-        cmdList.setDepthBias(light->constantBias(), light->slopeBias());
-
-        moodycamel::ConcurrentQueue<DrawCallDescription> drawCalls {};
-
-        auto& instances = scene.staticMeshInstances();
-        ParallelForBatched(instances.size(), 256, [&](size_t idx) {
-            auto& instance = instances[idx];
-
-            if (const StaticMesh* staticMesh = scene.staticMeshForInstance(*instance)) {
-
-                if (!staticMesh->hasNonTranslucentSegments()) {
-                    return;
-                }
-
-                // TODO: Pick LOD properly
-                const StaticMeshLOD& lod = staticMesh->lodAtIndex(0);
-
-                ark::aabb3 aabb = staticMesh->boundingBox().transformed(instance->transform().worldMatrix());
-                if (lightFrustum.includesAABB(aabb)) {
-
-                    for (u32 segmentIdx = 0; segmentIdx < lod.meshSegments.size(); ++segmentIdx) {
-                        StaticMeshSegment const& meshSegment = lod.meshSegments[segmentIdx];
-
-                        // Don't render translucent objects. We still do masked though and pretend they are opaque. This may fail
-                        // in some cases but in general if the masked features are small enough it's not really noticable.
-                        if (meshSegment.blendMode == BlendMode::Translucent) {
-                            continue;
-                        }
-
-                        DrawCallDescription drawCall = meshSegment.vertexAllocation.asDrawCallDescription();
-                        drawCall.firstInstance = instance->drawableHandleForSegmentIndex(segmentIdx).indexOfType<u32>(); // TODO: Put this in some buffer instead!
-
-                        drawCalls.enqueue(drawCall);
-                    }
-                }
-            }
-        });
-
-        DrawCallDescription drawCall;
-        while (drawCalls.try_dequeue(drawCall)) {
-            cmdList.issueDrawCall(drawCall);
-        }
-
-        cmdList.endRendering();
-    };
+RenderTarget& DirectionalLightShadowNode::makeRenderTarget(Registry& reg, LoadOp loadOp) const
+{
+    return reg.createRenderTarget({ { RenderTarget::AttachmentType::Depth, m_shadowMap, loadOp, StoreOp::Store } });
 }
--- arkose/rendering/nodes/DirectionalLightShadowNode.h
+++ arkose/rendering/nodes/DirectionalLightShadowNode.h
@@ -1,13 +1,22 @@
 #pragma once
 
-#include "rendering/RenderPipelineNode.h"
+#include "rendering/meshlet/MeshletDepthOnlyRenderNode.h"
 
-class DirectionalLightShadowNode final : public RenderPipelineNode {
+class DirectionalLightShadowNode final : public MeshletDepthOnlyRenderNode {
 public:
     std::string name() const override { return "Directional light shadow"; }
     void drawGui() override;
 
     ExecuteCallback construct(GpuScene&, Registry&) override;
+
+protected:
+    bool usingDepthBias() const override { return true; }
+    vec2 depthBiasParameters(GpuScene&) const override;
+
+    mat4 calculateViewProjectionMatrix(GpuScene&) const override;
+    geometry::Frustum calculateCullingFrustum(GpuScene&) const override;
+
+    RenderTarget& makeRenderTarget(Registry&, LoadOp loadOp) const override;
 
 private:
     Texture* m_shadowMap { nullptr };
