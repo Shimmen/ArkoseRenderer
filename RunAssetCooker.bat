2286f0489a8e47f74b46f23d36785eb7bccba700
Simon Moos <simon.moos95@gmail.com>
2025-05-04 16:28:07 +0100

    Tools: add USD importing to the asset cooking pipeline

--- RunAssetCooker.bat
+++ RunAssetCooker.bat
@@ -30,6 +30,10 @@ cmake --build build --target ArkAssetBakeTool --config Release
 cmake --build build --target ImgAssetBakeTool --config Release
 cmake --build build --target CopyFileTool --config Release
 
+IF NOT "%PXR_USD_LOCATION%"=="" (
+    cmake --build build --target UsdImportTool --config Release
+)
+
 echo(
 echo ====================================
 echo ====== Launching AssetCooker! ======
