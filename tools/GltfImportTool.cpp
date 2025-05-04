d775340e46a25f274c4a77e7791ea9cae105159a
Simon Moos <simon.moos95@gmail.com>
2025-05-04 16:46:09 +0100

    Tools: improve output dependency writing for glTF import tool

--- tools/GltfImportTool.cpp
+++ tools/GltfImportTool.cpp
@@ -44,24 +44,31 @@ int main(int argc, char* argv[])
         // Not needed / breaks the build in AssetCooker if this is included. Not sure why..
         //dependencyData += fmt::format("INPUT: {}\n", inputAsset);
 
+        auto addOutputDependency = [&dependencyData]<typename AssetType>(AssetType const& asset) {
+            // TODO: Ensure all meshes/images/etc. are relative to `targetDirectory`!
+            // TODO: Write paths relative to the 'imported' directory!
+            std::string dependencyPath = asset.assetFilePath().generic_string();
+            dependencyData += fmt::format("OUTPUT: {}\n", dependencyPath);
+        };
+
         for (auto const& image : result.images) {
-            dependencyData += fmt::format("OUTPUT: {}\n", image->assetFilePath());
+            addOutputDependency(*image);
         }
 
         for (auto const& material : result.materials) {
-            dependencyData += fmt::format("OUTPUT: {}\n", material->assetFilePath());
+            addOutputDependency(*material);
         }
 
         for (auto const& mesh : result.meshes) {
-            dependencyData += fmt::format("OUTPUT: {}\n", mesh->assetFilePath());
+            addOutputDependency(*mesh);
         }
 
         for (auto const& skeleton : result.skeletons) {
-            dependencyData += fmt::format("OUTPUT: {}\n", skeleton->assetFilePath());
+            addOutputDependency(*skeleton);
         }
 
         for (auto const& animation : result.animations) {
-            dependencyData += fmt::format("OUTPUT: {}\n", animation->assetFilePath());
+            addOutputDependency(*animation);
         }
 
         FileIO::writeTextDataToFile(dependencyFile, dependencyData);
