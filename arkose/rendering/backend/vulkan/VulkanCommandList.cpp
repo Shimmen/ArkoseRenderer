499728d795bf893f8530a0ff1fb4df58404cd5dd
Simon Moos <simon.moos95@gmail.com>
2025-08-10 20:55:21 +0100

    Vulkan: set `currentLayout` correctly after clearing a previously undefined image

--- arkose/rendering/backend/vulkan/VulkanCommandList.cpp
+++ arkose/rendering/backend/vulkan/VulkanCommandList.cpp
@@ -105,33 +105,39 @@ void VulkanCommandList::clearTexture(Texture& genTexture, ClearValue clearValue)
         vkCmdClearColorImage(m_commandBuffer, texture.image, VK_IMAGE_LAYOUT_GENERAL, &clearColor, 1, &range);
     }
 
-    if (originalLayout.has_value() && originalLayout.value() != VK_IMAGE_LAYOUT_UNDEFINED && originalLayout.value() != VK_IMAGE_LAYOUT_PREINITIALIZED) {
-        VkImageMemoryBarrier imageBarrier = { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER };
-        imageBarrier.oldLayout = VK_IMAGE_LAYOUT_GENERAL;
-        imageBarrier.newLayout = originalLayout.value();
-        imageBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-        imageBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    // Return the image to its original layout, if needed
+    if (originalLayout.has_value()) {
+        if (originalLayout.value() == VK_IMAGE_LAYOUT_UNDEFINED || originalLayout.value() == VK_IMAGE_LAYOUT_PREINITIALIZED) {
+            // Can't return it to undefined, let's keep it in general now
+            texture.currentLayout = VK_IMAGE_LAYOUT_GENERAL;
+        } else {
+            VkImageMemoryBarrier imageBarrier = { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER };
+            imageBarrier.oldLayout = VK_IMAGE_LAYOUT_GENERAL;
+            imageBarrier.newLayout = originalLayout.value();
+            imageBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+            imageBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
 
-        imageBarrier.image = texture.image;
-        imageBarrier.subresourceRange.aspectMask = aspectMask;
-        imageBarrier.subresourceRange.baseMipLevel = 0;
-        imageBarrier.subresourceRange.levelCount = texture.mipLevels();
-        imageBarrier.subresourceRange.baseArrayLayer = 0;
-        imageBarrier.subresourceRange.layerCount = texture.layerCount();
+            imageBarrier.image = texture.image;
+            imageBarrier.subresourceRange.aspectMask = aspectMask;
+            imageBarrier.subresourceRange.baseMipLevel = 0;
+            imageBarrier.subresourceRange.levelCount = texture.mipLevels();
+            imageBarrier.subresourceRange.baseArrayLayer = 0;
+            imageBarrier.subresourceRange.layerCount = texture.layerCount();
 
-        // FIXME: Probably overly aggressive barriers!
+            // FIXME: Probably overly aggressive barriers!
 
-        VkPipelineStageFlags sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-        imageBarrier.srcAccessMask = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
+            VkPipelineStageFlags sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+            imageBarrier.srcAccessMask = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
 
-        VkPipelineStageFlags destinationStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
-        imageBarrier.dstAccessMask = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
+            VkPipelineStageFlags destinationStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+            imageBarrier.dstAccessMask = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
 
-        vkCmdPipelineBarrier(m_commandBuffer,
-                             sourceStage, destinationStage, 0,
-                             0, nullptr,
-                             0, nullptr,
-                             1, &imageBarrier);
+            vkCmdPipelineBarrier(m_commandBuffer,
+                                 sourceStage, destinationStage, 0,
+                                 0, nullptr,
+                                 0, nullptr,
+                                 1, &imageBarrier);
+        }
     }
 }
 
