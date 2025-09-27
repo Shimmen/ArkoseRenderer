f2e5f0420ca9a6d713c560faf2a82e300fe92018
Simon Moos <simon.moos95@gmail.com>
2025-09-28 00:54:03 +0100

    Tools: replace CopyFileTool with Asset Cooker's built-in "CopyFile" command

--- RunAssetCooker.bat
+++ RunAssetCooker.bat
@@ -28,7 +28,6 @@ cmake --build build --target GltfImportTool --config Release
 cmake --build build --target IESConvertTool --config Release
 cmake --build build --target ArkAssetBakeTool --config Release
 cmake --build build --target ImgAssetBakeTool --config Release
-cmake --build build --target CopyFileTool --config Release
 
 IF NOT "%PXR_USD_LOCATION%"=="" (
     cmake --build build --target UsdImportTool --config Release
